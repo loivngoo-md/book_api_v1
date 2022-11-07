3d1143f8acb4bf59e5f790bae0b3c908a0d645d8
Loi Ngo Van <loivngoo@Lois-MacBook-Air.local>
2022-11-07 09:34:29 +0700

    added

--- src/routes/ApiRouter.ts
+++ src/routes/ApiRouter.ts
@@ -1,8 +1,10 @@
 import { AuthRouter } from "./auth/AuthRouter";
+import * as express from "express";
 
 import { ServiceRouter } from "./services";
 
 import { BaseRouter } from "./base";
+import { HttpStatusCode } from "../app/enums";
 
 class ApiRouter extends BaseRouter {
   constructor() {
@@ -14,6 +16,21 @@ class ApiRouter extends BaseRouter {
     this._router.use("/services/app", new ServiceRouter()._router);
 
     this._router.use("/TokenAuth", new AuthRouter()._router);
+
+    this._router.get("/mode/view", async  (
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction
+    ) => {
+      try {
+        return res.status(HttpStatusCode.OK).json({
+          status: 200,
+          mode: 1,
+        });
+      } catch (error) {
+        next(error);
+      }
+    })
   }
 }
 
